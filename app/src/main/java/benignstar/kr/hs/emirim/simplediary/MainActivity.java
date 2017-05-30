abe8b57434c282f4a87fd4a0eddcb4ca1a1c6904
benignstar <che0009@naver.com>
2017-05-30 20:45:49 +0900

    #3 현재날짜를 DatePicker에 설정

--- app/src/main/java/benignstar/kr/hs/emirim/simplediary/MainActivity.java
+++ app/src/main/java/benignstar/kr/hs/emirim/simplediary/MainActivity.java
@@ -1,5 +1,6 @@
 package benignstar.kr.hs.emirim.simplediary;
 
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -7,7 +8,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    DatePicker data;
+    DatePicker date;
     EditText edit;
     Button but;
     String file_name;
@@ -16,10 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        data=(DatePicker)findViewById(R.id.data);
+        date=(DatePicker)findViewById(R.id.date);
         edit=(EditText)findViewById(R.id.edit);
         but=(Button)findViewById(R.id.but);
 
+        Calendar cal=Calendar.getInstance();
+        int year=cal.get(Calendar.YEAR);
+        int month=cal.get(Calendar.MONTH);
+        int day=cal.get(Calendar.DATE);
 
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                file_name=Integer.toString(i)+"_"+Integer.toString(i1+1)+"_"+Integer.toString(i2)+".txt";
+                String readData=readDiary(file_name);
+                edit.setText(readData);
+                but.setEnabled(true);
+            }
+        });
+
+
+    }
+    public String readDiary(String file_name){
+
+        return null;
     }
 }
