576c67f0e44dcea27c36e4cf156d3ba18485424f
benignstar <che0009@naver.com>
2017-05-30 20:24:32 +0900

    #2 View 객체의 참조값 설정하기

--- app/src/main/java/benignstar/kr/hs/emirim/simplediary/MainActivity.java
+++ app/src/main/java/benignstar/kr/hs/emirim/simplediary/MainActivity.java
@@ -2,12 +2,24 @@ package benignstar.kr.hs.emirim.simplediary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker data;
+    EditText edit;
+    Button but;
+    String file_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data=(DatePicker)findViewById(R.id.data);
+        edit=(EditText)findViewById(R.id.edit);
+        but=(Button)findViewById(R.id.but);
+
+
     }
 }
